ec58ca4dd778d1fe4c0bee05022eb0b944374cb3
Liia Dulher <dulher@ucu.edu.ua>
2022-02-23 14:05:32 +0200

    change main for client

--- src/main.py
+++ src/main.py
@@ -2,24 +2,30 @@ from src.client.client import Client
 
 
 def main():
-    liia = Client("Liia Dulher")
-    liia.add_web_server("http://127.0.0.1:8000/")
-    res = liia.post_message("Hello")
-    if res[0] != 200:
-        print(res[1])
-        return
-    res = liia.get_data()
-    print(res[1])
-    if res[0] != 200:
-        return
-    res = liia.post_message("world")
-    if res[0] != 200:
-        print(res[1])
-        return
-    res = liia.get_data()
-    print(res[1])
-    if res[0] != 200:
-        return
+    client_name = input("Enter client's name: ")
+    client = Client(client_name)
+    client.add_web_server("http://127.0.0.1:8000/")
+    text = "start"
+    while text.lower() != "end":
+        print("Please choose one of the options or 'end' to exit")
+        text = input("'Post' or 'Get': ")
+        if text.lower() == "post":
+            msg = input("Enter message:")
+            res = client.post_message(msg)
+            if res[0] != 200:
+                print("Error:", res[1])
+            else:
+                print("Message successfully posted.")
+        elif text.lower() == "get":
+            res = client.get_data()
+            if res[0] != 200:
+                print("Error:", res[1])
+            else:
+                print(res[1])
+                print()
+        else:
+            print("Please choose one of the options or 'end' to exit")
+    return
 
 
 if __name__ == "__main__":
