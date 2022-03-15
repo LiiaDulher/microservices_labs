e75021f2e9b09c43634b7063aa552ad3a98ee9c4
Liia Dulher <dulher@ucu.edu.ua>
2022-03-15 17:24:22 +0200

    servers runner using cmd; imports

--- src/services/run/logging_server_run.py
+++ src/services/run/logging_server_run.py
@@ -1,12 +1,29 @@
 from src.services.app.logging_service import LoggingServer
 from src.data.data_storage import DataStorage
 
+import sys
+
 
 def main():
+    n = len(sys.argv)
+    if n == 1:
+        number = 1
+        facade_server = "http://127.0.0.1:8000/"
+        host = "127.0.0.1"
+        port = 8001
+    elif n == 5:
+        number = sys.argv[1]
+        host = sys.argv[2]
+        port = sys.argv[3]
+        facade_server = sys.argv[4]
+    else:
+        print("Wrong arguments number. Use default arguments or give yours in such order:\n"
+              "server number, server host, server port, facade server url")
+        return
     storage = DataStorage()
-    server = LoggingServer(1, storage)
-    server.add_facade_server("http://127.0.0.1:8000/")
-    server.run("127.0.0.1", 8001)
+    server = LoggingServer(number, storage)
+    server.add_facade_server(facade_server)
+    server.run(host, port)
 
 
 if __name__ == "__main__":
