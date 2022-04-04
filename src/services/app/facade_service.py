d4737c31b1179d4c8e99ef2956f81cabf51e8129
Liia Dulher <dulher@ucu.edu.ua>
2022-04-04 14:25:06 +0300

    facade service improvement

--- src/services/app/facade_service.py
+++ src/services/app/facade_service.py
@@ -14,45 +14,10 @@ class FacadeServer(Server):
         def facade():
             if request.method == 'POST':
                 msg = request.json["msg"]
-                i = 0
-                data = {"uuid": self.uuid + 1, "msg": msg}
-                try:
-                    response = requests.post(self.log_server, json=data)
-                except requests.exceptions.RequestException as err:
-                    print("Logging service error:", err)
-                    return Response("Internal Service Error", 500)
-                while i < 10:
-                    if response.status_code == 200:
-                        break
-                    elif response.status_code == 409:
-                        self.uuid += 10
-                        i += 1
-                    else:
-                        print("Logging service POST request error:", response.status_code, response.text)
-                        return Response("Internal Service Error", 500)
-                    data = {"uuid": self.uuid + 1, "msg": msg}
-                    try:
-                        response = requests.post(self.log_server, json=data)
-                    except requests.exceptions.RequestException as err:
-                        print("Logging service error:", err)
-                        return Response("Internal Service Error", 500)
-                self.uuid += 1
-                return Response("Message successfully posted", 200)
+                return self.post_request(self.log_server, msg)
             elif request.method == 'GET':
-                try:
-                    response1 = requests.get(self.msg_server)
-                except requests.exceptions.RequestException as err:
-                    print("Message service error:", err)
-                    return Response("Internal Server Error", 500)
-                try:
-                    response2 = requests.get(self.log_server)
-                except requests.exceptions.RequestException as err:
-                    print("Logging service error:", err)
-                    return Response("Internal Server Error", 500)
-                if response1.status_code != 200:
-                    print("Message service GET request error:", response1.status_code, response1.text)
-                if response2.status_code != 200:
-                    print("Logging service GET request error:", response2.status_code, response2.text)
+                response1 = self.get_request(self.msg_server, "Message")
+                response2 = self.get_request(self.log_server, "Logging")
                 if response1.status_code != 200 or response2.status_code != 200:
                     return Response("Internal Server Error", 500)
                 text = response2.text + response1.text
@@ -64,3 +29,42 @@ class FacadeServer(Server):
     def add_messages_server(self, msg_path):
         self.msg_server = msg_path
 
+    def remove_logging_server(self, log_path):
+        pass
+
+    def post_request(self, server, msg):
+        i = 0
+        data = {"uuid": self.uuid + 1, "msg": msg}
+        try:
+            response = requests.post(server, json=data)
+        except requests.exceptions.RequestException as err:
+            print("Logging service error:", err)
+            return Response("Internal Service Error", 500)
+        while i < 10:
+            if response.status_code == 200:
+                break
+            elif response.status_code == 409:
+                self.uuid += 10
+                i += 1
+            else:
+                print("Logging service POST request error:", response.status_code, response.text)
+                return Response("Internal Service Error", 500)
+            data = {"uuid": self.uuid + 1, "msg": msg}
+            try:
+                response = requests.post(server, json=data)
+            except requests.exceptions.RequestException as err:
+                print("Logging service error:", err)
+                return Response("Internal Service Error", 500)
+        self.uuid += 1
+        return Response("Message successfully posted", 200)
+
+    def get_request(self, server, server_type):
+        try:
+            response = requests.get(server)
+        except requests.exceptions.RequestException as err:
+            print(server_type, "service error:", err)
+            return Response("Internal Server Error", 500)
+        if response.status_code != 200:
+            print(server_type, "service GET request error:", response.status_code, response.text)
+            return Response("Internal Server Error", 500)
+        return response
