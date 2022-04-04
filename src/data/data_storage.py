a100346eb74afb0f12c4ffcde92ab9be1c1ef4b9
Liia Dulher <dulher@ucu.edu.ua>
2022-04-04 15:57:41 +0300

    add hz to storage, multi log services to runner

--- src/data/data_storage.py
+++ src/data/data_storage.py
@@ -5,21 +5,28 @@ class DataStorage:
     def __init__(self):
         # TODO: use dist_map not data
         # TODO: specify node
-        hz = hazelcast.HazelcastClient()
-        self.dist_map = hz.get_map("logging-service-distributed-map").blocking()
+        # hz = hazelcast.HazelcastClient()
+        # self.dist_map = hz.get_map("logging-service-distributed-map").blocking()
         self.data = {}
 
     def save_data(self, uuid, msg):
+        # if self.dist_map.contains_key(uuid):
+        #         #     raise KeyError("Such id already exists")
+        #         # self.dist_map.put(uuid, msg)
         if uuid in self.data.keys():
             raise KeyError("Such id already exists")
         self.data[uuid] = msg
 
     def get_data_by_id(self, uuid):
+        # if self.dist_map.contains_key(uuid):
+        #     return self.dist_map.get(uuid)
         if uuid in self.data.keys():
             return self.data[uuid]
         raise KeyError("Such id does not exist")
 
     def get_all_data(self):
+        # values = self.dist_map.values()
+        # return "\n".join(values)
         all_data = ""
         for key in self.data.keys():
             all_data += self.data[key] + "\n"
--- src/services/run/facade_server_run.py
+++ src/services/run/facade_server_run.py
@@ -8,19 +8,21 @@ def main():
     if n == 1:
         host = "127.0.0.1"
         port = 8000
-        logging_server = "http://127.0.0.1:8001/"
+        logging_servers = ["http://127.0.0.1:8001/"]
         messages_server = "http://127.0.0.1:8002/"
     elif n == 5:
         host = sys.argv[1]
         port = sys.argv[2]
-        logging_server = sys.argv[3]
+        logging_servers = sys.argv[3]
+        logging_servers = logging_servers.split(",")
         messages_server = sys.argv[4]
     else:
         print("Wrong arguments number. Use default arguments or give yours in such order:\n"
-              "server number, server host, server port, facade server url")
+              "server host, server port, logging servers urls(separated by coma not space), messages server url")
         return
     server = FacadeServer()
-    server.add_logging_server(logging_server)
+    for logging_server in logging_servers:
+        server.add_logging_server(logging_server)
     server.add_messages_server(messages_server)
     server.run(host, port)
 
